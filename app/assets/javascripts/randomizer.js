c3c49e12a6c94457dbc7a95d4bbf20b973b6b47d
Bryce <byoung1018@gmail.com>
2015-11-14 13:47:54 -0800

    add saving to form

--- app/assets/javascripts/randomizer.js
+++ app/assets/javascripts/randomizer.js
@@ -1,16 +1,28 @@
 (function () {
   $("#add-choice").on("click", addChoice);
   $("#pick-random").on("click", pickRandom);
-  function addChoice(event) {
-    var newChoice = $('<div class="choice-container"><input class="choice" type="text"></div>')
+  $("#list").on("click", ".remove-choice", removeChoice);
 
-    $("form.choices").append(newChoice);
+  function addChoice(event) {
+    var newChoice = $(
+      '<div class="choice-container">' +
+        '<input class="choice" name="list[choices][]" type="text">' +
+        '<button class="remove-choice">-</button>' +
+      '</div>')
+
+    $("form#list").append(newChoice);
   }
 
   function pickRandom(event) {
+    event.preventDefault();
     var choices = $("input.choice");
     var choice = randomElement(choices);
     $("h2.chosen").html(choice.value);
+  }
+
+  function removeChoice(event) {
+    event.preventDefault();
+    event.currentTarget.parentElement.remove();
   }
 
   function randomElement(choices) {
--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,6 +1,17 @@
 class ListsController < ApplicationController
   def create
-    puts params
+    # check current user for id
+    list = List.new(owner_id: current_user.id, name: list_params[:name])
+    list.save!
+    list_params[:choices].each do |text|
+      choice = Choice.new(owner_id: current_user.id, text: text)
+      choice.save!
+      ListItem.new(list_id: list.id, choice_id: choice.id).save!
+    end
+  end
+
+  def list_params
+    params.require(:list).permit(:name, choices: [])
   end
 
 end
