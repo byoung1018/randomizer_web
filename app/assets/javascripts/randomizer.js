1a40feb5507f1bb6a864e558f032fbc66f82a167
Bryce Young <bryce@hired.com>
2016-10-22 13:43:14 -0700

    Add single line parsing

--- app/assets/javascripts/randomizer.js
+++ app/assets/javascripts/randomizer.js
@@ -1,34 +1,47 @@
 (function () {
-  $("#add-choice").on("click", addChoice);
-  $("#pick-random").on("click", pickRandom);
-  $("#list").on("click", ".remove-choice", removeChoice);
+    $("#add-choice").on("click", addChoice);
+    $("#pick-random").on("click", pickRandom);
+    $("#list").on("click", ".remove-choice", removeChoice);
 
-  function addChoice(event) {
-    var newChoice = $(
-      '<div class="choice-container">' +
-        '<input class="choice" name="list[choices][]" type="text">' +
-        '<button class="remove-choice">-</button>' +
-      '</div>')
+    function addChoice(event) {
+        var newChoice = $(
+            '<div class="choice-container">' +
+            '<input class="choice" name="list[choices][]" type="text">' +
+            '<button class="remove-choice">-</button>' +
+            '</div>')
 
-    $("form#list").append(newChoice);
-  }
+        $("form#list").append(newChoice);
+    }
 
-  function pickRandom(event) {
-    event.preventDefault();
-    var choices = $("input.choice");
-    var choice = randomElement(choices);
-    $("h2.chosen").html(choice.value);
-  }
+    function pickRandom(event) {
+        event.preventDefault();
+        var choices = getChoices();
+        var choice = randomElement(choices);
+        console.log("this");
+        $("h2.chosen").html(choice);
+    }
 
-  function removeChoice(event) {
-    event.preventDefault();
-    event.currentTarget.parentElement.remove();
-  }
 
-  function randomElement(choices) {
-    var num = Math.random();
-    num = Math.floor(num * choices.length);
-    return choices[num];
-  }
+
+    function getChoices() {
+        var choices = $("input.choice");
+        Array.prototype.flatten = function () {
+            return [].concat.apply([], this)
+        }
+        choices = choices.map(function (){return this.value.split(' or ');})
+        choices = [].concat.apply([], choices).map(function (line) {return line.split(',');})
+        return [].concat.apply([], choices).map(function(choice){return choice.trim();})
+    }
+
+    function removeChoice(event) {
+        event.preventDefault();
+        event.currentTarget.parentElement.remove();
+    }
+
+    function randomElement(choices) {
+        var num = Math.random();
+        num = Math.floor(num * choices.length);
+        return choices[num];
+    }
 
 })();
